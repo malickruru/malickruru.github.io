eb3b01c82c5175c586c18e363057a2f842335f71
Rubens Mensah <85185634+malickruru@users.noreply.github.com>
2022-03-03 06:33:41 +0000

    Update MesProjet.js

--- script/MesProjet.js
+++ script/MesProjet.js
@@ -10,21 +10,21 @@ const projets = {
         {
             Nom:"7develop.com",
             Description:"Site web d'une agence IT",
-            Image:"../img/7develop.png",
+            Image:"img/7develop.png",
             lien_code_source:null,
             lien:"https://7develop.com"
         },
         {
             Nom:"7goto.net",
             Description:"Page d'accueil d'une agence d'assistance en ligne",
-            Image:"../img/7goto.png",
+            Image:"img/7goto.png",
             lien_code_source:null,
             lien:"https://7goto.net"
         },
         {
             Nom:"Qr code scanner",
             Description:"Application permettant de scanner un qr code",
-            Image:"../img/qrcode.png",
+            Image:"img/qrcode.png",
             lien_code_source:"https://github.com/malickruru/qrcode_lecteur",
             lien:"https://7develop.com/2021/7scanning2021v01/wbqr.htm"
         }
@@ -33,7 +33,7 @@ const projets = {
         {
             Nom:"Wordle",
             Description:"Deviner un le mot caché",
-            Image:"../img/wordle.png",
+            Image:"img/wordle.png",
             lien_code_source:"https://malickruru.github.io/wordle/",
             lien:"https://github.com/malickruru/wordle"
         }
